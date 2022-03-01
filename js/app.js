276234e400f006ee41fdc3adcf636f8e598f2a8f
iqbalhossainraju <iqbalhossainraju@yahoo.com>
2022-03-02 00:07:09 +0600

    view all btn added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 const searchItem = () => {
     const searchItem = document.getElementById('search-field');
     const itemDetails = document.getElementById('item-details');
+    const viewButton = document.querySelector('.view-all-btn');
+    viewButton.style.display = 'none';
     const searchText = searchItem.value;
     searchItem.value = '';
     itemDetails.textContent = '';
+    const brandName = document.querySelector('.brand-name');
+    brandName.textContent= searchText;
     if(searchText == ''){
         swal("Input is Empty!", "please write what you want?", "warning");
     }
@@ -12,19 +16,33 @@ const searchItem = () => {
 
         fetch(url)
         .then(response => response.json())
-        .then(data => displayedSearchItem(data.data))
+        .then(data => {
+            displayedSearchItem(data.data);
+            if(data.data.length > 20){
+                viewButton.style.display = 'block';
+            }
+        })
     }
 
 }
 
-const displayedSearchItem = (phones) => {
+const displayedSearchItem = (phones, viewAll = false) => {
     if(phones.length == 0) {
         swal("Wrong Input!", "please write what you want?", "error");
     }
     else {
         const searchResult = document.getElementById('search-result');
         searchResult.textContent = '';
-        const showtwentyphones = phones.slice(0, 20)
+        let showtwentyphones;
+ if(viewAll === false){
+    if(phones.length > 20){
+        showtwentyphones =  phones.slice(0,20);
+   }else if(phones.length < 20){
+       showtwentyphones = phones;
+   }
+ }else{
+       showtwentyphones = phones;
+   }
         showtwentyphones.forEach(phone => {
             const div =document.createElement('div');
             div.classList.add('col');
@@ -46,6 +64,7 @@ const displayedSearchItem = (phones) => {
     }
 }
 
+
 const productsDetails = (id) => {
     const url =  `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url).then(res => res.json())
@@ -63,8 +82,44 @@ const productsDetailsDisplayed = (details) => {
     <div class="card-body">
       <h5 class="card-title">Name: ${details.name}</h5>
       <h6 class="card-title">Release Date: ${details.releaseDate}</h6>
-      <p>sensor: ${details.mainFeatures.sensors[0]},<br> ${details.mainFeatures.sensors[1]}, ${details.mainFeatures.sensors[2]}</p>
+      <p>
+      Sensor:
+            ${details.mainFeatures.sensors[0]},<br>
+            ${details.mainFeatures.sensors[1]},<br>
+            ${details.mainFeatures.sensors[2]},<br>
+            ${details.mainFeatures.sensors[3]},<br>
+            ${details.mainFeatures.sensors[4]},<br>
+            ${details.mainFeatures.sensors[5]},<br>
+        </p>
+      <p>
+      ${details.others ? 'Others: = ' : ''}
+            ${details?.others?.Bluetooth ? details?.others?.Bluetooth :  ""}<br>
+            ${details?.others?.GPS ? details?.others?.GPS : ""}<br>
+            ${details?.others?.Bluetooth ? details?.others?.Bluetooth : ""}<br>
+            ${details?.others?.NFC ? details?.others?.NFC : ""}<br>
+            ${details?.others?.Radio ? details?.others?.Radio : ""}<br>
+            ${details?.others?.USB ? details?.others?.USB : ''}<br>
+            ${details?.others?.WLAN ? details?.others?.WLAN :  ""}
+        </p>
     `;
     itemDetails.appendChild(div)
 }
+
+const viewAll = () => {
+    const searchItem = document.querySelector('.brand-name');
+    const itemDetails = document.getElementById('item-details');
+    const viewButton = document.querySelector('.view-all-btn');
+    viewButton.style.display = 'none';
+    const searchText = searchItem.textContent;
+    itemDetails.textContent = '';
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            displayedSearchItem(data.data, true);
+        })
+   
+}
+
 
